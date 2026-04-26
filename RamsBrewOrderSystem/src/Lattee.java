90ff9f85468babe187ab09555a133ad4a5321cc8
MangoDuck26 <dannytraylor326@gmail.com>
2026-04-26 17:14:25 -0400

    Add Lattee class implementing Beverage Interface

--- RamsBrewOrderSystem/src/Lattee.java
+++ RamsBrewOrderSystem/src/Lattee.java
@@ -2,22 +2,31 @@ public class Latte implements Beverage {
     private String size;
     private String milk;
     private String sweetener;
+    private int shots;
 
-    public Latte(String size, String milk, String sweetener) {
+    public Latte(String size, String milk, String sweetener, int shots) {
         this.size = size;
         this.milk = milk;
         this.sweetener = sweetener;
+        this.shots = shots;
     }
 
     @Override
     public String getInfo() {
-        return "Your Latte is a " + size + " with " + milk + " milk and " + sweetener;
+        return "Your Latte is a " + size + " with" + shots + " shot(s) and " + milk + " milk, and " +  sweetener;
     }
 
     @Override
     public double getCost() {
-        if (size.equalsIgnoreCase("Small")) return 3.00;
-        else if (size.equalsIgnoreCase("Medium")) return 3.50;
-        else return 4.00;
+        double base;
+        if (size.equalsIgnoreCase("Small")) {
+            base = 3.00;
+        } else if (size.equalsIgnoreCase("Medium")) {
+            base = 3.50;
+        } else {
+            base = 4.00;
+        }
+        base += shots * 0.50; // shot cost
+        return base;
     }
 }
