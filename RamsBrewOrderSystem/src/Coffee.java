c9f8f5f0700c58baad0aeb6a933d39a1bd50ca3b
MangoDuck26 <dannytraylor326@gmail.com>
2026-04-26 16:40:03 -0400

    Add Coffee class implementing Beverage interface

---
+++ RamsBrewOrderSystem/src/Coffee.java
@@ -0,0 +1,11 @@
+public class Coffee implements beverage{
+    private String size;
+    private String milk;
+    private String sweetener;
+
+    public Coffee(String size, String milk, String sweetener){
+        this.size = size;
+        this.milk = milk;
+        this.sweetener = sweetener;
+    }
+}
